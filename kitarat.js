0b97bb8713c27448bad5bbfe580206ec5036162f
edd-star <eddtherizzler@gmail.com>
2025-11-12 11:10:49 +0200

    alotusta

---
+++ kitarat.js
@@ -0,0 +1,6 @@
+const tuotteet = [
+    { id: 1, name: "Epiphone Les Paul Custom EB", price: 139.00, img: "kuvat/lespaul.png" },
+    { id: 2, name: "Jackson JS32 King V BLK AH", price: 29.90, img: "kuvat/jackson_JS32_kingv.png" },
+    { id: 3, name: "Harley benton - kitara setti", price: 150.00, img: "kuvat/kitaraset.png" },
+    { id: 4, name: "vinyylilevysoitin", price: 269.90, img: "kuvat/levysoitin.png" },
+]
